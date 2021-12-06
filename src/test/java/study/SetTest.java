9c5a6453a201eb6f1cd85ac43c6c7c0ca7bead14
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-06 21:19:57 +0900

    feat: Set 요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -34,5 +35,11 @@ public class SetTest {
 	@DisplayName("contains test")
 	void containsTest(int value) {
 		assertThat(numbers.contains(value)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void containsTestCsvSource(int number, boolean expected) {
+		assertThat(numbers.contains(number)).isEqualTo(expected);
 	}
 }
