b0bfcfda42b83ef2df8c0ecf5c9645cf079a5987
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-06 21:14:11 +0900

    feat: Set 요구사항 2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 	private Set<Integer> numbers;
@@ -25,5 +27,12 @@ public class SetTest {
 	@DisplayName("요구사항1: Set 크기 확인")
 	void setSizeTest() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("contains test")
+	void containsTest(int value) {
+		assertThat(numbers.contains(value)).isTrue();
 	}
 }
