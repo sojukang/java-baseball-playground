b15e225bbcd1956c1788690553ac18f1a3b84b93
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-06 14:13:12 +0900

    feat: 요구사항 1, 2 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,36 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
+	@Test
+	void replace() {
+		String actual = "abc".replace("b", "d");
+		assertThat(actual).isEqualTo("adc");
+	}
+
+	@Test
+	@DisplayName("(,) split test")
+	void splitTest() {
+		String[] actual = "1,2".split(",");
+		assertThat(actual).isEqualTo(new String[] {"1", "2"});
+	}
+
+	@Test
+	@DisplayName("1 split test")
+	void splitNoTest() {
+		String[] actual = "1".split(",");
+		assertThat(actual).isEqualTo(new String[] {"1"});
+	}
+
     @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+    @DisplayName("()제거")
+    void removeParenthesis() {
+        String actual = "(1,2)";
+        actual = actual.substring(1, actual.length()-1);
+        assertThat(actual).isEqualTo("1,2");
     }
 }
